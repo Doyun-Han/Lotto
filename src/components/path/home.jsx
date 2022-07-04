20f367e7d71eec92fb5a9ec9dbbf1b63fbcd0bce
Han <dy__z@nate.com>
2022-07-04 18:59:36 +0900

    modify Css to fit Ad

--- src/components/path/home.jsx
+++ src/components/path/home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { React } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { secondGame, randomGame, sortNum } from '../../data/data';
@@ -5,7 +6,7 @@ import Button from '../button/button';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import Info from '../info/info';
-
+import styles from './home.module.css'
 const Home = ({addGame}) => {
     const navigate = useNavigate();
 
@@ -26,12 +27,32 @@ const Home = ({addGame}) => {
     const gotoMain = () => {
         navigate('/main')
     }
+
+    useEffect(()=> {
+        let ins = document.createElement('ins');
+        let scr = document.createElement('script');
+
+        ins.className = 'kakao_ad_area';
+        ins.style = "display:none;";
+        scr.async = 'true';
+        scr.type = "text/javascript";
+        scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
+        ins.setAttribute('data-ad-width', '320');
+        ins.setAttribute('data-ad-height', '100');
+        ins.setAttribute('data-ad-unit', 'DAN-aMwDDc45vvdBFbJl');
+
+        document.querySelector('.adfit').appendChild(ins);
+        document.querySelector('.adfit').appendChild(scr);
+    })
     return(
         <>
+        <div className={styles.container}>
         <Header />
         <Info />
         <Button setGame={setGame}/>
         <Footer />
+        </div>
+        <div className="adfit"/>
         </>
     )
 }
